fb5173a0209ae82103feb59e7e8523b7aba6d5b2
chanhee01 <joch4267@naver.com>
2024-07-19 23:31:00 +0900

    feat : plug 도메인 생성

---
+++ src/main/java/com/example/IotController/domain/Device/model/Plugs.java
@@ -0,0 +1,29 @@
+package com.example.IotController.domain.Device.model;
+
+import com.example.IotController.domain.User.model.Users;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Plugs {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    private Boolean status; // 지금 장치가 사용중인지 아닌지
+
+    private Boolean mode; // 자동 제어 모드
+
+    private Long time; // 자동 제어 기준 시간
+}
