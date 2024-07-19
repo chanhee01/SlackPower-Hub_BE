8c1ae3724b77dc733868c22686a59fc5962b5ab0
chanhee01 <joch4267@naver.com>
2024-07-20 00:42:52 +0900

    feat : Plugs 엔티티에 builer 추가

--- src/main/java/com/example/IotController/domain/Device/model/Plugs.java
+++ src/main/java/com/example/IotController/domain/Device/model/Plugs.java
@@ -2,6 +2,7 @@ package com.example.IotController.domain.Device.model;
 
 import com.example.IotController.domain.User.model.Users;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,4 +27,13 @@ public class Plugs {
     private Boolean mode; // 자동 제어 모드
 
     private Long time; // 자동 제어 기준 시간
+
+    @Builder
+    public Plugs(String name, Users user, Long time) {
+        this.name = name;
+        this.user = user;
+        this.status = false;
+        this.mode = false;
+        this.time = time;
+    }
 }
